9430e77021bc84e19afe98339a6a9ccd2f56d635
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-06-25 10:30:44 +0300

    +

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { renderCard } from './js/render-function';
 const searchSettings = {
   key: '44443472-3b41bcc651e7d0b56b1888f38',
   q: '',
@@ -30,39 +31,4 @@ function fetchImage(qveryURL) {
     }
     return response.json();
   });
-}
-
-function renderCard(imageData) {
-  return imageData
-    .map(
-      el =>
-        `<li class="card">
-            <a href="${el.largeImageURL}" class="big gallery-link">
-              <img
-                src="${el.previewURL}"
-                alt="${el.tags}"
-                title="${el.tags}"
-                class="card-img"
-            /></a>
-            <ul class="card-title">
-              <li class="card-text-blok">
-                <h2 class="card-title-text">Likes</h2>
-                <p class="card-text-value">${el.likes}</p>
-              </li>
-              <li class="card-text-blok">
-                <h2 class="card-title-text">Views</h2>
-                <p class="card-text-value">${el.views}</p>
-              </li>
-              <li class="card-text-blok">
-                <h2 class="card-title-text">Comments</h2>
-                <p class="card-text-value">${el.comments}</p>
-              </li>
-              <li class="card-text-blok">
-                <h2 class="card-title-text">Downloads</h2>
-                <p class="card-text-value">${el.downloads}</p>
-              </li>
-            </ul>
-          </li>`
-    )
-    .join('');
 }
